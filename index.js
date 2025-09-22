6c30f7e7d65c5d6e82ca92211a8059345b5d2ae1
Shibam Prasad Bhagat <shibambhagat03@gmail.com>
2025-09-22 12:09:09 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Namaste ');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
