8171184570256954872647d7fadc556d7bf01ca7
Shibam Prasad Bhagat <shibambhagat03@gmail.com>
2025-09-24 13:14:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hi shibam ');
+  res.send('Hi Cloudies');
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
