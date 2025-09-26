6f2c644e2a654bf30150e3752f36c63a2bc5a6d8
Shibam Prasad Bhagat <shibambhagat03@gmail.com>
2025-09-26 14:13:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080; // Correctly setting the port
 
 app.get("/", (req, res) => {
     // Ensuring a 200 OK response for the Health Check Path (/)
-    res.status(200).send("Now its working!");
+    res.status(200).send("Hi Cloudies");
 });
 
 // CRITICAL FIX: Explicitly bind the server to 0.0.0.0
